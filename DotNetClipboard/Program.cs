87c2c8a2b12c63b63ac8cb06433beae54793dcf8
Kevin FOO <oofnivek@gmail.com>
2025-02-06 16:12:36 +0800

    make port configurable in appsettings.json

--- DotNetClipboard/Program.cs
+++ DotNetClipboard/Program.cs
@@ -13,7 +13,11 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 // Configure port to listen on
 builder.WebHost.ConfigureKestrel(options =>
 {
-    options.ListenAnyIP(port: 5432);
+    var port = builder.Configuration.GetValue<int>("Kestrel:Port"); // Get port from appsettings.json
+    if (port == 0) {
+        port = 8080; // Default port if not in appsettings.json
+    }
+    options.ListenAnyIP(port);
 });
 
 var app = builder.Build();
